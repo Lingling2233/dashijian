63b42c80aa62665b696880c405540c3a7ffca683
jiangshiquan <1454652779@qq.com>
2021-01-02 14:17:19 +0800

    新增退出功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,11 +9,53 @@ function getUserInfo() {
         method: "GET",
         url: "/my/userinfo",
         // headers就是请求头
-        headers: {
-            Authorization: localStorage.getItem("token") || ""
-        },
+        // headers: {
+        //     Authorization: localStorage.getItem("token") || ""
+        // },
         success: function (res) {
-            console.log(res);
+            // console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg("获取用户信息失败")
+            }
+            // 调用渲染用户的头像
+            renderAvatar(res.data)
         }
     })
 }
+function renderAvatar(user) {
+    // 获取用户名称
+    var name = user.nickname || user.username
+    // 设置欢迎的文本
+    $("#welcome").html("欢迎&nbsp;&nbsp;" + name)
+    // 渲染用户头像
+    if (user.user_pic !== null) {
+        $(".layui-nav-img").attr("src", user.user_pic).show()
+        $(".text-avatar").hide()
+    } else {
+        // 渲染文本头像
+        $(".layui-nav-img").hide()
+        // 获取第一个字符，并转换成大写
+        var first = name[0].toUpperCase()
+        $(".text-avatar").html(first).show()
+    }
+}
+var layer = layui.layer
+// 设置退出
+$("#btnLogout").on("click", function () {
+    // console.log(1);
+    layer.confirm('is not?', { icon: 3, title: '提示' }, function (index) {
+        //do something
+
+        layer.close(index);
+    });
+    //eg2
+    layer.confirm('确认退出?', function (index) {
+        //do something
+        // console.log(1);
+        localStorage
+        // 清除本地的token
+        localStorage.removeItem("token")
+        location.href ="../../login.html"
+            layer.close(index);
+    });
+})
