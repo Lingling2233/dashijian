7b53b3b6607d3e2b38b5dbe6cbbedffc22b2571e
jiangshiquan <1454652779@qq.com>
2021-01-03 17:32:14 +0800

    完成了个人中心的开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,70 +1,74 @@
 $(function () {
+    getUserInfo()
 
+    var layer = layui.layer
+    // 设置退出
+    $("#btnLogout").on("click", function () {
+        // console.log(1);
+
+        layer.confirm('确认退出?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            // console.log(1);
+
+            // 清除本地的token
+            localStorage.removeItem("token")
+            location.href = "../../login.html"
+            layer.close(index);
+        })
+    })
 })
 
 
-getUserInfo()
+
+// 获取用户的基本信息
 function getUserInfo() {
     $.ajax({
-        method: "GET",
-        url: "/my/userinfo",
-        // headers就是请求头
-        // headers: {
-        //     Authorization: localStorage.getItem("token") || ""
-        // },
-        success: function (res) {
-            // console.log(res);
-            if (res.status !== 0) {
-                return layui.layer.msg("获取用户信息失败")
-            }
-            // 调用渲染用户的头像
-            renderAvatar(res.data)
-        },
-        // 不论登录成功还是失败，都会调用complete
-        complete: function (res) {
-            // console.log(1);
-            if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
-                localStorage.removeItem("token")
-                location.href = "../../login.html"
-            }
+      method: 'GET',
+      url: '/my/userinfo',
+      success: function(res) {
+        if (res.status !== 0) {
+          return layui.layer.msg('获取用户信息失败！')
         }
-
+        // 调用 renderAvatar 渲染用户的头像
+        //   console.log(res.data);
+          
+        renderAvatar(res.data)
+      }
+      // 不论成功还是失败，最终都会调用 complete 回调函数
+      // complete: function(res) {
+      //   // console.log('执行了 complete 回调：')
+      //   // console.log(res)
+      //   // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+      //   if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+      //     // 1. 强制清空 token
+      //     localStorage.removeItem('token')
+      //     // 2. 强制跳转到登录页面
+      //     location.href = '/login.html'
+      //   }
+      // }
     })
-}
-function renderAvatar(user) {
-    // 获取用户名称
-    var name = user.nickname || user.username
-    // 设置欢迎的文本
-    $("#welcome").html("欢迎&nbsp;&nbsp;" + name)
-    // 渲染用户头像
-    if (user.user_pic !== null) {
-        $(".layui-nav-img").attr("src", user.user_pic).show()
-        $(".text-avatar").hide()
-    } else {
-        // 渲染文本头像
-        $(".layui-nav-img").hide()
-        // 获取第一个字符，并转换成大写
-        var first = name[0].toUpperCase()
-        $(".text-avatar").html(first).show()
-    }
-}
-var layer = layui.layer
-// 设置退出
-$("#btnLogout").on("click", function () {
-    // console.log(1);
-    layer.confirm('is not?', { icon: 3, title: '提示' }, function (index) {
-        //do something
+  }
 
-        layer.close(index);
-    });
-    //eg2
-    layer.confirm('确认退出?', function (index) {
-        //do something
-        // console.log(1);
-        localStorage
-        // 清除本地的token
-        localStorage.removeItem("token")
-        location.href = "../../login.html"
-        layer.close(index);
-    });
-})
+
+  function renderAvatar(user) {
+    // 1. 获取用户的名称
+    var name = user.nickname || user.username
+    // 2. 设置欢迎的文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    // 3. 按需渲染用户的头像
+    if (user.user_pic !== null) {
+      // 3.1 渲染图片头像
+      $('.layui-nav-img')
+        .attr('src', user.user_pic)
+        .show()
+      $('.text-avatar').hide()
+    } else {
+      // 3.2 渲染文本头像
+      $('.layui-nav-img').hide()
+      var first = name[0].toUpperCase()
+      $('.text-avatar')
+        .html(first)
+        .show()
+    }
+  }
+
